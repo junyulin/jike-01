bc625ff60ab328a32a17a47bad86915bd5c50a68
linjn <443082762@qq.com>
2021-04-27 09:18:07 +0800

    添加一些字节码测试文件和工具包

---
+++ Foo.java
@@ -0,0 +1,14 @@
+public class Foo {
+
+    public static void main(String[] args) {
+        boolean flag = true;
+		float f = 10.3F;
+        if (flag) {
+            System.out.println("Hello, Java!");
+        }
+        if (flag == true) {
+            System.out.println("Hello, JVM!");
+        }
+    }
+
+}
